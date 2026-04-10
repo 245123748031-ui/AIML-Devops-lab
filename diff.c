c8f85382a1b4188abbe4f2e4780f480c06ef7c40
245123748031-ui <245123748031@mvsrec.edu.in>
2026-04-10 12:42:17 +0530

    x

---
+++ diff.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int diff = a - b;
+    printf("The difference of %d and %d is %d\n", a, b, diff);
+    return 0;
+}
---
+++ div.py
@@ -0,0 +1,9 @@
+# Input: two numbers
+numerator = 10
+denominator = 3
+
+# Perform division
+result = numerator / denominator
+
+# Output the result
+print("The result of true division is:", result)
